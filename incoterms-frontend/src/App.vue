aef990676d4d7c3eaf2ef6c5977baf339cd26cf9
xMiguelGonzalez <migueelgonzaalezz@gmail.com>
2026-05-17 23:17:29 +0200

    minor_changes

--- incoterms-frontend/src/App.vue
+++ incoterms-frontend/src/App.vue
@@ -29,7 +29,7 @@ const listaIncoterms = ref([]);
 const listaPasos = ref([]);
 const incotermAEditar = ref(null);
 
-const URL_API = 'http://127.0.0.1:8000/api';
+const URL_API = 'http://api-miguel.51.83.192.177.nip.io/api';
 
 const cargarPasos = async () => {
 
